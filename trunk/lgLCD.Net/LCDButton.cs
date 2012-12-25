d6565bbaa8bbcf90b6c8cf9b56e8b25fa61101c2
cr42y7je <cr42y7je@e523a450-6ed6-439c-a432-41fc96dd629e>
2012-12-25 14:24:55 +0000

    Initial version

---
+++ trunk/lgLCD.Net/LCDButton.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LgLCD.Net {
+    /// <summary>
+    /// Enum with the LCD buttons
+    /// </summary>
+    public enum LCDButton {
+        BUTTON_1 = 1,
+        BUTTON_2 = 2,
+        BUTTON_3 = 4,
+        BUTTON_4 = 8
+    }
+}
