e7aece829707253478e03c739e19899e808e1786
cr42y7je <cr42y7je@e523a450-6ed6-439c-a432-41fc96dd629e>
2013-01-12 11:57:03 +0000

    Added dll's that were missing + a sample app

---
+++ Samples/Sample1/Sample1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using LgLCD.Net;
+using System.Drawing;
+
+namespace Sample1 {
+    class Program {
+        private static bool _blnIsColorDisplay = false;
+        [STAThread]
+        static void Main() {
+
+            LcdManager.Init("MyApplet", true, GetBitmap);
+            Application.Run();
+        }
+
+        private static IntPtr GetBitmap() {
+            Bitmap objBitmap;
+            if (_blnIsColorDisplay) {
+                objBitmap = new Bitmap(320, 240);
+            } else {
+                objBitmap = new Bitmap(160, 43);
+            }
+
+            //create an empty Bitmap
+            Graphics g = Graphics.FromImage(objBitmap);
+            g.Clear(Color.White);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            g.DrawString("My Applet", new Font("Arial", 14, FontStyle.Regular), SystemBrushes.WindowText, 0, 20);
+
+            return objBitmap.GetHbitmap();
+        }
+    }
+}
